cde8ac93877216443788d6330ac50c3161833e8d
root <root@ip-172-31-46-100.us-west-2.compute.internal>
2015-10-29 02:37:45 +0000

    send email

---
+++ send_attachment.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.utils import parseaddr, formataddr
+import smtplib
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr(( \
+                          Header(name, 'utf-8').encode(), \
+                          addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+
+
+from_addr = raw_input('Email: ')
+password = raw_input('Password: ')
+to_addr = '1016601657@qq.com'
+smtp_server = 'smtp.163.com'
+
+# 邮件对象:
+msg = MIMEMultipart()
+msg['From'] = _format_addr(u'Python爱好者 <%s>' % from_addr)
+msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
+msg['Subject'] = Header(u'来自SMTP的问候……', 'utf-8').encode()
+
+# 邮件正文是MIMEText:
+msg.attach(MIMEText('send with file...', 'plain', 'utf-8'))
+
+# 添加附件就是加上一个MIMEBase，从本地读取一个图片:
+file = raw_input('Filename: ')
+with open(file, 'rb') as f:
+    # 设置附件的MIME和文件名，这里是png类型:
+    mime = MIMEBase('image', 'png', filename='test.png')
+    # 加上必要的头信息:
+    mime.add_header('Content-Disposition', 'attachment', filename='test.png')
+    mime.add_header('Content-ID', '<0>')
+    mime.add_header('X-Attachment-Id', '0')
+    # 把附件的内容读进来:
+    mime.set_payload(f.read())
+    # 用Base64编码:
+    encoders.encode_base64(mime)
+    # 添加到MIMEMultipart:
+    msg.attach(mime)
+
+
+server = smtplib.SMTP(smtp_server, 25)
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
